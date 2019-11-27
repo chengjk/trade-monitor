2c58ce54e999b87098c2c38497818e4661aece7f
jacky <jacky.cheng@yeahmobi.com>
2019-11-27 17:00:22 +0800

    mv private config to private

--- src/main/java/com/jk/trademonitor/TradeMonitorApplication.java
+++ src/main/java/com/jk/trademonitor/TradeMonitorApplication.java
@@ -2,8 +2,10 @@ package com.jk.trademonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@PropertySource("classpath:private.properties")
 @EnableScheduling
 @SpringBootApplication
 public class TradeMonitorApplication {
